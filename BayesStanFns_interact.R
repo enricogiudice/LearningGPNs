c72ae2b1551077d2a04ff1ddbe310d0819c32a60
enricogiudice <83815622+enricogiudice@users.noreply.github.com>
2023-05-12 14:45:24 +0200

    Update BayesStanFns_interact.R

--- BayesStanFns_interact.R
+++ BayesStanFns_interact.R
@@ -3,8 +3,8 @@ library(bridgesampling)
 options(mc.cores = parallel::detectCores())
 
 # Compile stan files
-Gauss_mod <- stan_model("/Users/giudic0000/Downloads/Nonlinear scoring/Structure Learning/Gauss.stan") 
-GP_int_mod <- stan_model("/Users/giudic0000/Downloads/Nonlinear scoring/Structure Learning/Add_interact.stan") 
+Gauss_mod <- stan_model("Gauss.stan") 
+GP_int_mod <- stan_model("Add_interact.stan") 
 
 # Laplace approximation of log marginal likelihood
 Laplace <- function(optim) {
