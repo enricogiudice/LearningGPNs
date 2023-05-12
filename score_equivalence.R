f2b18bd2edbde1ba044b63ab308f1ee7d2e905da
enricogiudice <83815622+enricogiudice@users.noreply.github.com>
2023-05-12 14:57:00 +0200

    Update score_equivalence.R

--- score_equivalence.R
+++ score_equivalence.R
@@ -2,11 +2,11 @@
 library(tidyverse)
 library(BiDAG)
 
-source("/Users/giudic0000/Downloads/Nonlinear scoring/Structure Learning/Fourier_fns.R")
-source("/Users/giudic0000/Downloads/Nonlinear scoring/Structure Learning/BayesStanFns.R")
-source("/Users/giudic0000/Downloads/Nonlinear scoring/Structure Learning/sampling_fns.R")
-source("/Users/giudic0000/Downloads/Nonlinear scoring/Structure Learning/dualPC.R")
-insertSource("~/Downloads/Nonlinear scoring/Structure Learning/GPscore.R", package = "BiDAG")
+source("Fourier_fns.R")
+source("BayesStanFns.R")
+source("sampling_fns.R")
+source("dualPC.R")
+insertSource("GPscore.R", package = "BiDAG")
 
 # Compute score of DAG
 GPscoreDAG <- function(dag, data) {
